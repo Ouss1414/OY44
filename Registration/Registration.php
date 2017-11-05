890b53f7d2f5194865ebbf5bf68290fdd8176156
OuSs <oussama.almalawi@hotmail.com>
2017-11-05 15:12:21 +0300

    Like And Dislike

--- Registration/Registration.php
+++ Registration/Registration.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['user'])) {
     <link rel="stylesheet" href="../CSS/w3-theme-blue-grey.css">
     <link href="https://bootswatch.com/yeti/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/BootStrap.css">
-    <link rel=\'stylesheet\' href=\'https://fonts.googleapis.com/css?family=Open+Sans\'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
